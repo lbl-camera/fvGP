1e4a57bd3f38b2cea9924dd276a169796a4c320a
Marcus Michael Noack <MarcusNoack@lbl.gov>
2020-11-23 14:45:02 -0800

    minor

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -5,7 +5,7 @@ from test_fvgp import TestfvGP
 def main():
     a = TestfvGP()
     ###choose a function for testing here
-    a.test_1d_single_task(training_method = "global")
+    a.test_1d_single_task(training_method = "global", N = 1000)
     #a.test_1d_single_task(training_method = "hgdl")
     #a.test_us_topo(method = "hgdl",dask_client = True)
     #a.test_us_topo(method = "global",dask_client = False)
