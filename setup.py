7f061bf604a913a21358431aa723fbb618f5d95b
Marcus Michael Noack <MarcusNoack@lbl.gov>
2021-07-15 09:51:54 -0700

    version change

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     packages=find_packages(include=['fvgp', 'fvgp.*']),
     test_suite='tests',
     url='https://github.com/MarcusMichaelNoack/fvgp',
-    version='3.0.1',
+    version='3.0.2',
     zip_safe=False,
 )
