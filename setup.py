c878a0a7738d07dd9bba8c724a741fabd74abd9b
Marcus Michael Noack <MarcusNoack@lbl.gov>
2021-11-24 14:48:47 -0800

    requirements updated

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     packages=find_packages(include=['fvgp', 'fvgp.*']),
     test_suite='tests',
     url='https://github.com/MarcusMichaelNoack/fvgp',
-    version='3.2.4',
+    version='3.2.5',
     zip_safe=False,
 )
