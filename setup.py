116849f623feb1b28b7a307c63760c99b1650fe7
Ronald Pandolfi <ronpandolfi@gmail.com>
2022-01-19 16:27:05 -0800

    update readme path

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os import path
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
