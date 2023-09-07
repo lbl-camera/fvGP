67c69543d2178c3accb668cd0ad353d0ddded9ac
Ronald Pandolfi <ronpandolfi@gmail.com>
2023-09-06 17:01:54 -0700

    promote public classes to package top-level

--- fvgp/__init__.py
+++ fvgp/__init__.py
@@ -6,7 +6,10 @@ __email__ = 'MarcusNoack@lbl.gov'
 from . import _version
 from loguru import logger
 import sys
+from .gp import GP
+from .fvgp import fvGP
 
+__all__ = ['GP', 'fvGP']
 __version__ = _version.get_versions()['version']
 
 logger.disable('fvgp')
