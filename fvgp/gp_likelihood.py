error fix in posterior covariance

fvgp/gp_likelihood.py
@@ -72,7 +72,8 @@ class GPlikelihood:
         return noise
 
     def _measured_noise_function(self, x, hyperparameters):
-        return self.noise_variances
+        if len(x) == len(self.noise_variances): return self.noise_variances
+        else: return np.zeros((len(x))) + 0.00001
 
     @staticmethod
     def _default_dnoise_dh(x, hps):

fvgp/gp_posterior.py
@@ -118,10 +118,10 @@ class GPposterior:
 
         if add_noise and callable(self.likelihood_obj.noise_function):
             noise = self.likelihood_obj.noise_function(x_pred, self.prior_obj.hyperparameters)
-            if issparse(noise): noise = noise.toarray()
+            assert isinstance(noise, np.ndarray) and np.ndim(noise) == 1
             if len(x_pred) == len(noise):
-                v = v + np.diag(noise)
-                if S is not None: S = S + noise
+                v = v + noise
+                if S is not None: S = S + np.diag(noise)
             else:
                 warnings.warn("Noise could not be added, you did not provide a noise callable at initialization")
 